4476d9d3bdab277caec0a2a8d14903905d8ce5f4
Nitesh Bhandari <nitesh@vayavya.co.in>
2020-09-12 18:53:18 +0530

    * Added delete, init and clone functionality * Some tweaks to internal variables * Help comes by default * Shows error for unsupported command * Environment variable set for condition checking is namespaced and unset * Unstaging is changed to command 'us' instead of 'rm', which now maps to 'git rm'

--- g.cmd
+++ g.cmd
@@ -1,18 +1,22 @@
 @echo off
-SET oneline=
-if x%1==xh GOTO help
-if x%1==xa GOTO add
-if x%1==xc GOTO commit
-if x%1==xs GOTO status
-if x%1==xpl GOTO pull
-if x%1==xps GOTO push 
-if x%1==xl GOTO log
-if x%1==xl1 GOTO logOneline
-if x%1==xt GOTO test
-if x%1==xr GOTO remote_list
+if x%1==xh   GOTO help
+if x%1==x    GOTO help
+if x%1==xa   GOTO add
+if x%1==xc   GOTO commit
+if x%1==xs   GOTO status
+if x%1==xpl  GOTO pull
+if x%1==xps  GOTO push 
+if x%1==xl   GOTO log
+if x%1==xl1  GOTO logOneline
+if x%1==xt   GOTO test
+if x%1==xr   GOTO remote_list
 if x%1==xrgs GOTO remote_set_url
-if x%1==xrg GOTO remote_github
-if x%1==xrm GOTO remove_from_staged
+if x%1==xrg  GOTO remote_github
+if x%1==xus  GOTO unstage
+if x%1==xrm  GOTO delete
+if x%1==xi   GOTO init
+if x%1==xcl  GOTO clone
+echo Invalid command. Type 'g h' to get help
 GOTO exit
 
 ##############################################################
@@ -34,9 +38,12 @@ GOTO exit
 @echo     g l       : git log
 @echo     g l1      : git log --oneline
 @echo     g r       : git remote -v
-@echo     g rgs     : git remote set-url github "<repo_url>"
-@echo     g rg      : git remote add github "<repo_url>" and set it as default remote for master
-@echo     g rm      : git restore --staged [file1] [file2] [file3] ... (remove from staged)
+@echo     g rgs     : git remote set-url github <repo_url>
+@echo     g rg      : git remote add github <repo_url> : Also sets it as default remote for master
+@echo     g us      : git restore --staged [file1] [file2] [file3] ... : Unstage files
+@echo     g rm      : git rm [file1] [file2] [file3] ... : Delete files
+@echo     g i       : git init
+@echo     g cl      : git clone <repo_url> [target_dir]
 @echo     g h       : This help message
 @echo off
 GOTO exit
@@ -77,12 +84,14 @@ git push
 GOTO exit
 
 ##############################################################
+SET GIT_CMD_ONELINE=
 :logOneline
-SET oneline=--oneline
+SET GIT_CMD_ONELINE=--oneline
 :log
 @echo on
-git log %oneline%
+git log %GIT_CMD_ONELINE%
 @echo off
+SET GIT_CMD_ONELINE=
 GOTO exit
 
 ##############################################################
@@ -109,9 +118,30 @@ git config branch.master.merge refs/heads/master
 GOTO exit
 
 ##############################################################
-:remove_from_staged
+:unstage
 @echo on
 git restore --staged %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
+GOTO exit
+
+##############################################################
+:delete
+@echo on
+git rm %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
+GOTO exit
+
+##############################################################
+:init
+@echo on
+git init
+@echo off
+GOTO exit
+
+##############################################################
+:clone
+@echo on
+git clone %2 %3
 @echo off
 GOTO exit
 
